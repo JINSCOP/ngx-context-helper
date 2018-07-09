1da9de0da799bd88c8159efa7a13a63aed908f6a
金飞 <28206254@qq.com>
2018-07-09 10:20:06 +0800

    s

--- dist/README.md
+++
@@ -1,1 +0,0 @@
-# ngx-context-helper
--- dist/src/ngx-context-helper.component.d.ts
+++
@@ -1,8 +0,0 @@
-import { OnInit, EventEmitter } from '@angular/core';
-export declare class NgxContextHelperComponent implements OnInit {
-    dataSource: any;
-    settings: any;
-    test: EventEmitter<any>;
-    constructor();
-    ngOnInit(): void;
-}
